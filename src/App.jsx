1e19bd8881b703b2722db52fe0598e8280e610b5
BrendenSeifried <brendenseifried@gmail.com>
2022-05-04 17:41:18 -0700

    setting up pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Auth from './views/Auth/Auth';
+import GuestBook from './views/GuestBook/GuestBook';
+import Home from './views/Home/Home';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <PrivateRoute path="/guestbook">
+          <GuestBook />
+        </PrivateRoute>
+      </Switch>
+    </BrowserRouter>
+  );
 }
---
+++ src/views/Auth/Auth.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Auth() {
+  return (
+    <div>
+      Auth
+      <input placeholder="Enter a UserName" />
+      <input placeholder="Enter a PassWord" />
+    </div>
+  );
+}
